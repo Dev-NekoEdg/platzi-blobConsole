37342eb6a3e86ec0a70f7fef71e011353aff86e7
Edgard Alvarado <edgard-0211@hotmail.com>
2023-02-22 13:43:04 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace platzi_blobConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+            string getstringConn = config.GetSection("ConnectionStrings").Value;
+
+            // Console.WriteLine("Hello World!");
+            Console.WriteLine(getstringConn);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Platzi-BlobConsole
+
+applicacion de consola hecha en VS code para probar y hacer ejecicios de curso [Almacenamiento en Azure](https://platzi.com/cursos/almacenamiento-azure/)
+
+
+
+----
+## Nuget Packages
+
+* dotnet add package Microsoft.Extensions.Configuration --version 7.0.0
+* dotnet add package Microsoft.Extensions.Configuration.FileExtensions --version 7.0.0
+* dotnet add package Microsoft.Extensions.Configuration.Json --version 7.0.0
+
+----
+**Comandos consola dotnet**
+|Comando |Descripción |
+|:---- |:---- |
+|`dotnet new console -n NombrePryecto` |Crea el proyecto de tipo consola con el nombre. |
+
+
