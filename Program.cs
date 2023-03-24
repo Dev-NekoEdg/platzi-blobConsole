a493c7c8828efdbdaf4598c47f1cc8c69e61b537
Edgard Alvarado <Edgard-0211@hotmail.com>
2023-03-24 17:02:14 -0500

    modify to extension for all course

--- Program.cs
+++ Program.cs
@@ -12,38 +12,53 @@ namespace platzi_blobConsole
     {
         static void Main(string[] args)
         {
+            // UploadFile();
+        }
+
+        
+
+        private void UploadFile()
+        {
             var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json");
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.Local.json"); // cambiar a appsettings.
 
             // IConfiguration config = new ConfigurationBuilder()
             // .AddJsonFile("appsettings.json", true, true)
             // .Build();
             var config = builder.Build();
 
-            string getstringConn = config.GetSection("ConnectionStrings").Value;
             string getstringConn1 = config["pruebas"];
 
             // Console.WriteLine("Hello World!");
-            Console.WriteLine(getstringConn);
             Console.WriteLine(getstringConn1);
 
+            Console.WriteLine("Conexion de Azure");
             string getstring = config["AzureConnection"];
 
             CloudStorageAccount cuentaAlmacenamiento = CloudStorageAccount.Parse(config["AzureConnection"]);
             CloudBlobClient client = cuentaAlmacenamiento.CreateCloudBlobClient();
+
             // los containers no aceptan mayusculas.
             CloudBlobContainer container = client.GetContainerReference("containernuevo");
+            // Crea el container si no existe.
             container.CreateIfNotExists();
+
+            // Colocar permisos para poder subir imagenes.
             container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
 
-            CloudBlockBlob miBlod = container.GetBlockBlobReference("imagetest.jpg");
-            string imageRoot = @"C:\Users\Edgar\OneDrive\Pictures\usuario.png";
+            string newName = Guid.NewGuid().ToString() + ".jpg";
+            CloudBlockBlob miBlod = container.GetBlockBlobReference(newName);
+            // string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            string baseFolderImg = config["baseImageRoot"]; ;
+            string imageRoot = Path.Combine(baseFolderImg, "FXgvpruWIAE4uNr.jpg");
 
             using (var file = System.IO.File.OpenRead(imageRoot))
             {
                 miBlod.UploadFromStream(file);
             }
+
         }
     }
 }
